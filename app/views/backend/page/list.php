ad01ccdca24228671fdc5e4251b9dc35e1c65b1e
phuoctrank51a6 <56340109+phuoctrank51a6@users.noreply.github.com>
2019-11-25 22:22:01 +0700

    update

--- app/views/backend/page/list.php
+++ app/views/backend/page/list.php
@@ -7,7 +7,7 @@ require_once './app/views/backend/master/sidebar.php';
   <div class="content">
     <div class="page-inner">
       <div class="page-header">
-        <h4 class="page-title">Xe</h4>
+        <h4 class="page-title">Page</h4>
         <ul class="breadcrumbs">
           <li class="nav-home">
             <a href="#">
@@ -18,7 +18,7 @@ require_once './app/views/backend/master/sidebar.php';
             <i class="flaticon-right-arrow"></i>
           </li>
           <li class="nav-item">
-            <a href="<?= ADMIN_URL . '/car' ?>">Xe</a>
+            <a href="<?= ADMIN_URL . '/car' ?>">Page</a>
           </li>
         </ul>
       </div>
